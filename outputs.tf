541e9153f3ffd2ba7be8ce8d3270c60ef3d8d24e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:28:19 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "active_directory_domain_service_trusts" {
+  description = "All active_directory_domain_service_trust resources"
+  value       = azurerm_active_directory_domain_service_trust.active_directory_domain_service_trusts
+}
+
