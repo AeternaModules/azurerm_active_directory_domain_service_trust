19862229f03146767af4850f077ba183e8ac49a3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:34 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.active_directory_domain_service_trusts : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_active_directory_domain_service_trust" "active_directory_domain_service_trusts" {
   for_each = var.active_directory_domain_service_trusts
 
   domain_service_id      = each.value.domain_service_id
   name                   = each.value.name
-  password               = each.value.password
+  password               = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
   trusted_domain_dns_ips = each.value.trusted_domain_dns_ips
   trusted_domain_fqdn    = each.value.trusted_domain_fqdn
 }
--- variables.tf
+++ variables.tf
@@ -5,16 +5,20 @@ Required:
     - domain_service_id
     - name
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
     - trusted_domain_dns_ips
     - trusted_domain_fqdn
 EOT
 
   type = map(object({
-    domain_service_id      = string
-    name                   = string
-    password               = string
-    trusted_domain_dns_ips = list(string)
-    trusted_domain_fqdn    = string
+    domain_service_id              = string
+    name                           = string
+    password                       = string
+    password_key_vault_id          = optional(string)
+    password_key_vault_secret_name = optional(string)
+    trusted_domain_dns_ips         = list(string)
+    trusted_domain_fqdn            = string
   }))
 }
 
